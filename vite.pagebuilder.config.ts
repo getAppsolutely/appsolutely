be6463e10ce5024d368b8908f37d61f0bc7f46d2
okchum <hi@okchum.com>
2025-10-15 08:51:40 +1300

    build(pagebuilder): increase chunk size warning limit to 2000

--- vite.pagebuilder.config.ts
+++ vite.pagebuilder.config.ts
@@ -61,6 +61,7 @@ export default defineConfig({
     },
     build: {
         assetsInlineLimit: 0,
+        chunkSizeWarningLimit: 2000,
         rollupOptions: {
             output: {
                 manualChunks: {
