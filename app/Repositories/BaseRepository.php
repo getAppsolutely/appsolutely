4c195e0542eaab9b4bd300f3cd712a09371934a8
okchum <hi@okchum.com>
2025-11-10 00:20:22 +1300

    refactor: add strict types to all Services and Repositories

--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Repositories;
 
 use Prettus\Repository\Eloquent\BaseRepository as Repository;
--- app/Services/Translation/OpenAITranslator.php
+++ app/Services/Translation/OpenAITranslator.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Services\Translation;
 
 use Illuminate\Support\Facades\Log;
