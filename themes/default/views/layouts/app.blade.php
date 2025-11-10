efd431b3648e9bdd8475b33aacc1d5c11e8bc384
okchum <hi@okchum.com>
2025-11-11 10:47:30 +1300

    fix: update Vite asset references from app.js to app.ts in default theme layouts

--- themes/default/views/layouts/app.blade.php
+++ themes/default/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['themes/default/css/app.css', 'themes/default/js/app.js'])
+        @vite(['themes/default/css/app.css', 'themes/default/js/app.ts'])
 
         <!-- Styles -->
         @livewireStyles
